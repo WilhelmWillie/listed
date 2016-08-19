f517334cdbca0c1cb30c595fa51682343af4eaad
Wilhelm Willie <wilhelmwillie@gmail.com>
2016-08-19 16:05:53 -0700

    Heroku config

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160731153145) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer  "list_id"
     t.integer  "user_id"
@@ -21,8 +24,8 @@ ActiveRecord::Schema.define(version: 20160731153145) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["list_id"], name: "index_comments_on_list_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+  add_index "comments", ["list_id"], name: "index_comments_on_list_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "items", force: :cascade do |t|
     t.integer  "list_id"
@@ -33,7 +36,7 @@ ActiveRecord::Schema.define(version: 20160731153145) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "items", ["list_id"], name: "index_items_on_list_id"
+  add_index "items", ["list_id"], name: "index_items_on_list_id", using: :btree
 
   create_table "likes", force: :cascade do |t|
     t.integer  "list_id"
@@ -42,8 +45,8 @@ ActiveRecord::Schema.define(version: 20160731153145) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "likes", ["list_id"], name: "index_likes_on_list_id"
-  add_index "likes", ["user_id"], name: "index_likes_on_user_id"
+  add_index "likes", ["list_id"], name: "index_likes_on_list_id", using: :btree
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
 
   create_table "lists", force: :cascade do |t|
     t.integer  "user_id"
@@ -55,17 +58,16 @@ ActiveRecord::Schema.define(version: 20160731153145) do
     t.integer  "comments_count"
   end
 
-  add_index "lists", ["user_id"], name: "index_lists_on_user_id"
+  add_index "lists", ["user_id"], name: "index_lists_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "display_name"
     t.string   "email"
+    t.string   "display_name"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
 
 end
